cae0a752b245644e84021b259f4b3f1195321b15
mamungithube <orroshidmdmamun50@gmail.com>
2024-07-25 21:31:41 +0600

    done

--- mamarbank/settings.py
+++ mamarbank/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
-import dj_database_url
 from pathlib import Path
+import dj_database_url
 
 import environ
 
